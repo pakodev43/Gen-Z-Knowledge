0929e14e98053d557a8b0b150b24b28bf91d109e
dev <dev@git.com>
2025-01-21 17:31:57 +0500

    Initial commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(cookieParser())
 

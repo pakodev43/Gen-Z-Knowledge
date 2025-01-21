4da2763bf7c1fc54c6ae3c2300c43e832e5c2336
dev <dev@git.com>
2025-01-21 16:03:09 +0500

    Initial commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/courses', coursesRoute)
 app.use('/', authRoute)
 
 // Run the server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     if (process.env.PHASE == 'development') {
         console.log(`Server is running on http://localhost:${process.env.PORT} in development`)
     }
